9482806aaa6c72810e8b50e3d2b71af2918b73f4
moshubai <939761443@qq.com>
2021-01-20 17:33:31 +0800

    学习

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,12 @@ import Home from '../views/Home.vue'
 const routes = [
   {
     path: '/',
+    redirect: {
+      name: 'Home'
+    }
+  },
+  {
+    path: '/',
     name: 'Home',
     component: Home
   },
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,68 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    Counter:{{ counter }}
+    <p>{{ dobCount }}</p>
+    <p>{{ objectOjb.bar }}</p>
+    <p>{{ objectOjb.foo }}</p>
+    <p ref="refsdom">我是ref</p>
+    <button @click="countFn">1</button>
   </div>
 </template>
+
+<script>
+import { onMounted, onBeforeMount, onUnmounted, onBeforeUnmount, ref, reactive, computed, watch } from 'vue'
+
+export default {
+  name: 'AboutPage',
+  components: {},
+  beforeCreate() {
+    console.log(1)
+  },
+  created() {
+    console.log(2)
+  },
+  setup() {
+    console.log('最最开始执行的')
+    //
+    const { counter } = dbCountFn()
+
+    const objectOjb = reactive({
+      bar: '1111',
+      foo: 222222
+    })
+    console.log(counter, objectOjb)
+
+    const countFn = () => {
+      counter.value++
+      objectOjb.bar = '99999999'
+    }
+
+    const dobCount = computed(() => counter.value * 2)
+
+    watch(counter, (newVal, oldVal) => {
+      console.log(newVal, oldVal, counter.value)
+    })
+
+    onBeforeMount(() => {
+      console.log(3)
+    })
+    onMounted(() => {
+      console.log(refsdom)
+      console.log(4)
+    })
+
+    onBeforeUnmount(() => {
+      console.log(5)
+    })
+    onUnmounted(() => {
+      console.log(6)
+    })
+    const refsdom = ref(null)
+    return { counter, countFn, objectOjb, dobCount, refsdom }
+  }
+}
+const dbCountFn = () => {
+  const counter = ref(1)
+  return { counter }
+}
+</script>
